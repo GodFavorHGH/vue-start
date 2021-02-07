7b2d563310fb25c011b73c9ca24404fd89622e12
GodFavorHGH <982859079@qq.com>
2021-02-07 22:33:21 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello" style="background: red;">
+    <router-view></router-view>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+          <button @click="hello">hello</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods :{
+    hello(){
+      const postData = {
+        startIndex:0,
+        pageSize:10
+      }
+      this.axios.post('http://localhost:9080/account/paginationQuery', postData).then(function (response) {
+          console.log(response.data);
+        }).catch(function (error) {
+          console.log(error.data);
+        });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
